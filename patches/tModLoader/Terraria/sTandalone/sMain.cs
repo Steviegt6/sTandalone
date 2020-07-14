c24928fc9001c3f6ee814bc11fb066db2af6d89c
Stevie <xxlennygamerxx@gmail.com>
2020-07-13 18:58:42 -0700

    welcome message

---
+++ patches/tModLoader/Terraria/sTandalone/sMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terraria.sTandalone
+{
+	public class sMain
+	{
+		public static void sTandalonePut() {
+			Main.Configuration.Put("ShowWelcomeMessage", Main._showWelcomeMessage);
+		}
+		public static void sTandaloneGet() {
+			Main.Configuration.Get("ShowWelcomeMessage", ref Main._showWelcomeMessage);
+		}
+	}
+}

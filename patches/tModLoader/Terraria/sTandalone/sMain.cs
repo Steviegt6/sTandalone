a2563060ed35d53edaebf3ae33423d3e677d528a
Stevie <xxlennygamerxx@gmail.com>
2020-07-14 19:46:47 -0700

    mods menu

--- patches/tModLoader/Terraria/sTandalone/sMain.cs
+++ patches/tModLoader/Terraria/sTandalone/sMain.cs
@@ -10,9 +10,18 @@ namespace Terraria.sTandalone
 	{
 		public static void sTandalonePut() {
 			Main.Configuration.Put("ShowWelcomeMessage", Main._showWelcomeMessage);
+			Main.Configuration.Put("UselessDontEditThis", Main.restartRequired);
+			Main.Configuration.Put("TerrariaPlus", Main.terrariaPlus);
 		}
 		public static void sTandaloneGet() {
 			Main.Configuration.Get("ShowWelcomeMessage", ref Main._showWelcomeMessage);
+			Main.Configuration.Get("UselessDontEditThis", false);
+			Main.Configuration.Get("TerrariaPlus", ref Main.terrariaPlus);
+
+			if (Main.terrariaPlus && !Main.enabledMods.Contains(Main.Mod.TerrariaPlus))
+				Main.enabledMods.Add(Main.Mod.TerrariaPlus);
+			else if(Main.enabledMods.Contains(Main.Mod.TerrariaPlus) && !Main.terrariaPlus)
+				Main.enabledMods.Remove(Main.Mod.TerrariaPlus);
 		}
 	}
 }

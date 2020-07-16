c1e1affc1094962b24c1129379f6afbc2a66fc99
Stevie <xxlennygamerxx@gmail.com>
2020-07-15 17:17:31 -0700

    probably some bugs but I need to sleep

--- patches/tModLoader/Terraria/sTandalone/sMain.cs
+++ patches/tModLoader/Terraria/sTandalone/sMain.cs
@@ -12,15 +12,21 @@ namespace Terraria.sTandalone
 			Main.Configuration.Put("ShowWelcomeMessage", Main._showWelcomeMessage);
 			Main.Configuration.Put("UselessDontEditThis", Main.restartRequired);
 			Main.Configuration.Put("TerrariaPlus", Main.terrariaPlus);
+			Main.Configuration.Put("FirstFractalRecipe", Main.firstFractalRecipe);
+			Main.Configuration.Put("SlowerMasterModeRarity", Main.slowerMasterModeRarity);
+			Main.Configuration.Put("AllAccessorySlotsInVanity", Main.allAccessoriesInVanitySlots);
 		}
 		public static void sTandaloneGet() {
 			Main.Configuration.Get("ShowWelcomeMessage", ref Main._showWelcomeMessage);
 			Main.Configuration.Get("UselessDontEditThis", false);
 			Main.Configuration.Get("TerrariaPlus", ref Main.terrariaPlus);
+			Main.Configuration.Get("FirstFractalRecipe", ref Main.firstFractalRecipe);
+			Main.Configuration.Get("SlowerMasterModeRarity", ref Main.slowerMasterModeRarity);
+			Main.Configuration.Get("AllAccessorySlotsInVanity", ref Main.allAccessoriesInVanitySlots);
 
 			if (Main.terrariaPlus && !Main.enabledMods.Contains(Main.Mod.TerrariaPlus))
 				Main.enabledMods.Add(Main.Mod.TerrariaPlus);
-			else if(Main.enabledMods.Contains(Main.Mod.TerrariaPlus) && !Main.terrariaPlus)
+			else if (Main.enabledMods.Contains(Main.Mod.TerrariaPlus) && !Main.terrariaPlus)
 				Main.enabledMods.Remove(Main.Mod.TerrariaPlus);
 		}
 	}

be74c06e23fa99b7311d831a58dae9e436450284
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-08-07 11:32:06 -0700

    Update TODO list, change some patcher values

--- setup/Properties/Settings.Designer.cs
+++ setup/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace Terraria.ModLoader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.4.0.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.4.0.5")]
         public string ClientVersion {
             get {
                 return ((string)(this["ClientVersion"]));
@@ -70,7 +70,7 @@ namespace Terraria.ModLoader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.4.0.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.4.0.5")]
         public string ServerVersion {
             get {
                 return ((string)(this["ServerVersion"]));
@@ -103,7 +103,7 @@ namespace Terraria.ModLoader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("17c42ca2c951a7a8a53ddf3a0ca00290")]
+        [global::System.Configuration.DefaultSettingValueAttribute("6b85b2a8ea384384c1090d784f3fcff6")]
         public string SteamClientWinMD5 {
             get {
                 return ((string)(this["SteamClientWinMD5"]));

0d8677158bf78e02a1f3aa6a5eded63990e20956
Stevie <xxlennygamerxx@gmail.com>
2020-09-21 21:31:32 -0700

    arms heads legs :devipoint:

--- patches/tStandalone/Terraria/ID/ArmorIDs.Standalone.cs
+++ patches/tStandalone/Terraria/ID/ArmorIDs.Standalone.cs
@@ -7,7 +7,6 @@
 			public const int MutantDevHat = 266;
 			public const int StevieDevHat = 267;
 			public const int Count = 268; // Vanilla: 266
-			// TODO: Replace 266 with ArmorIDs.Head.Count where applicable.
 		}
 
 		partial class Body
@@ -16,7 +15,6 @@
 			public const int StevieDevBody = 236;
 
 			public const int Count = 237; // Vanilla: 235
-			// TODO: Replace 235 with ArmorIDs.Body.Count where applicable.
 		}
 
 		partial class Legs
@@ -25,7 +23,6 @@
 			public const int StevieDevLegs = 219;
 
 			public const int Count = 218; // Vanilla: 218
-			// TODO: Replace 218 with ArmorIDs.Legs.Count where applicable.
 		}
 	}
 }

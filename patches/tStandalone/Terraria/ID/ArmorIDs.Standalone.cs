6aebac01ecdb16ccf2b766b4b7ea152cf01f481e
Stevie <xxlennygamerxx@gmail.com>
2020-09-21 21:42:08 -0700

    Update ArmorIDs.Standalone.cs

--- patches/tStandalone/Terraria/ID/ArmorIDs.Standalone.cs
+++ patches/tStandalone/Terraria/ID/ArmorIDs.Standalone.cs
@@ -22,7 +22,7 @@
 			public const int MutantDevLegs = 218;
 			public const int StevieDevLegs = 219;
 
-			public const int Count = 218; // Vanilla: 218
+			public const int Count = 219; // Vanilla: 218
 		}
 	}
 }
